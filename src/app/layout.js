98ed37d8ae77822d9ebe72a98c25237666a78267
Christian <christianfryksten1@gmail.com>
2025-06-01 21:36:10 +0200

    Update favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/cf-logo.png" sizes="any" />
+        <link rel="icon" href="/cf-logo.png" type="image/png" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
